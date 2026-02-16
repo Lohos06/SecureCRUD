541898d1af2025bf744a9c880dc70ceccc639357
Lohos06 <122732120+Lohos06@users.noreply.github.com>
2026-02-16 12:27:05 +0100

    Cheader

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HomePage</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <a href="index.php">HomePage</a>
+            <a href="inscription.php">inscription</a>
+            <a href="connexion.php">connexion</a>
+        </nav>
+    </header>
+</body>
+</html>
