2887a136b3603c9f7f6d06c377a5bc842fcf8999
Yasmine <meftah.yasmine2006@gmail.com>
2026-02-16 12:33:21 +0100

    inscription rempli

---
+++ inscription.php
@@ -0,0 +1,33 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['token_article_add']) || empty($_SESSION['token_article_add'])) {
+  $_SESSION['token_article_add'] = bin2hex(random_bytes(32));
+}
+?>
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Inscription</title>
+</head>
+
+<body>
+  <form action="traitement.php" method="post">
+    <input type="hidden" name="token" value="<?= $_SESSION['token_article_add']; ?>">
+    <label for="pseudo">Titre</label>
+    <input type="text" name="pseudo" id="pseudo">
+    <br>
+    <label for="slug">Slug</label>
+    <input type="text" name="slug" id="slug">
+    <br>
+    <label for="content">Contenu</label>
+    <textarea name="content" id="content"></textarea>
+    <br>
+    <button type="submit">Ajouter</button>
+  </form>
+</body>
+
+</html>
