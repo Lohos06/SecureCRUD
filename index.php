e7806661f07be82b7ad3555eee122831b663bb09
Lohos06 <122732120+Lohos06@users.noreply.github.com>
2026-02-16 12:50:04 +0100

    footer

--- index.php
+++ index.php
@@ -1,20 +1,32 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="/CSS/destyle.css" >
+
     <title>HomePage</title>
+    <meta name="description" content="A page to see what you are meant to">
+
 </head>
 <body>
     <header>
         <nav>
-            <a href="index.php">HomePage</a>
-            <a href="inscription.php">inscription</a>
-            <a href="connexion.php">connexion</a>
+            <a href="/index.php">HomePage</a>
+            <a href="/registration.php">registration</a>
+            <a href="/connection.php">connection</a>
         </nav>
     </header>
     <main>
         
     </main>
+    <footer>
+        <ul>
+            <li>Contact : Admin@gmail.com</li>
+            <li>author1 : Yasminemfth</li>
+            <li>author2 : Lohos</li>
+        </ul>
+    </footer>
 </body>
 </html>
