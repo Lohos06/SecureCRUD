cc96b6d032c30d61e78b878cec4217c310d23dca
Yasmine <meftah.yasmine2006@gmail.com>
2026-02-16 13:00:44 +0100

    ajout de biography

--- registration.php
+++ registration.php
@@ -16,23 +16,23 @@ if (!isset($_SESSION['token_article_add']) || empty($_SESSION['token_article_add
     <header>
         <nav>
             <a href="index.php">HomePage</a>
-            <a href="inscription.php">inscription</a>
-            <a href="connexion.php">connexion</a>
+            <a href="registration.php">inscription</a>
+            <a href="connection.php">connectiion</a>
         </nav>
     </header>
 <body>
   <form action="traitement.php" method="post">
     <input type="hidden" name="token" value="<?= $_SESSION['token_article_add']; ?>">
-    <label for="pseudo">Titre</label>
+    <label for="pseudo">Pseudo</label>
     <input type="text" name="pseudo" id="pseudo">
     <br>
-    <label for="slug">Slug</label>
-    <input type="text" name="slug" id="slug">
     <br>
-    <label for="content">Contenu</label>
-    <textarea name="content" id="content"></textarea>
+    <label for="password">Password</label>
+    <inpute name="password" id="password"></input>
+    <label for="biography">Biography</label>
+    <textarea name="biography" id="biography"></textarea>
     <br>
-    <button type="submit">Ajouter</button>
+    <button type="submit">Inscrire</button>
   </form>
 </body>
 
