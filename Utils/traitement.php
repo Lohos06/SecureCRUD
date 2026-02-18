24bcfe1de12e6ccac684a5e82427c9ae3e7341de
Yasmine <meftah.yasmine2006@gmail.com>
2026-02-18 10:00:41 +0100

    ajout verification pseudo existant et complexite mp dans traitement.php

--- Utils/traitement.php
+++ Utils/traitement.php
@@ -4,6 +4,7 @@ require_once "sessionStart.php";
 require_once "BDDAdmin.php";
 
 /* verif token*/
+
 if (
     !isset($_POST['token']) ||
     !isset($_SESSION['token_form_add']) ||
@@ -12,40 +13,62 @@ if (
     die('Erreur : Token invalide');
 }
 
-
 unset($_SESSION['token_form_add']);
 
 
-/* validation champs*/
+/*validation champs*/
 
 if (empty($_POST['pseudo'])) {
-    die('<p>Le pseudo est obligatoire</p>');
+    die('Le pseudo est obligatoire');
 }
 
 if (empty($_POST['password'])) {
-    die('<p>Le mot de passe est obligatoire</p>');
+    die('Le mot de passe est obligatoire');
 }
 
 if (empty($_POST['biography'])) {
-    die('<p>La biography est obligatoire</p>');
+    die('La biographie est obligatoire');
 }
 
-$pseudo = htmlspecialchars($_POST['pseudo']);
+$pseudo = htmlspecialchars(trim($_POST['pseudo']));
 $plainPassword = $_POST['password'];
-$biography = htmlspecialchars($_POST['biography']);
+$biography = htmlspecialchars(trim($_POST['biography']));
+
+/* Validation mp */
+$password_pattern = '/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&]).{8,}$/';
+
+if (!preg_match($password_pattern, $plainPassword)) {
+    die('Le mot de passe doit contenir au moins 8 caractères avec : 1 majuscule, 1 minuscule, 1 chiffre et 1 caractère spécial.');
+}
+
+
+/* connexion bdd*/
+
+try {
+    $pdo = new PDO(
+        'mysql:host=localhost;dbname=secureuser;charset=utf8',
+        'root',
+        ''
+    );
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die('Erreur BDD : ' . $e->getMessage());
+}
+
+
+/* verif pseudo*/
+
+$check = $pdo->prepare("SELECT id FROM users WHERE pseudo = ?");
+$check->execute([$pseudo]);
+
+if ($check->rowCount() > 0) {
+    die("Pseudo déjà utilisé.");
+}
 
 
 /* hash mp*/
 
 $hashedPassword = password_hash($plainPassword, PASSWORD_BCRYPT);
-
-echo "<p>$plainPassword devient $hashedPassword</p>";
-
-if (password_verify($plainPassword, $hashedPassword)) {
-    echo '<p>Les deux mots de passe correspondent</p>';
-} else {
-    echo '<p>Les deux mots de passe ne correspondent pas</p>';
-}
 
 
 /* insertion bdd*/
@@ -61,4 +84,4 @@ $insert->execute([
     'biography' => $biography
 ]);
 
-echo '<p>Inscription faite avec succès</p>';
+echo 'Inscription faite avec succès';
